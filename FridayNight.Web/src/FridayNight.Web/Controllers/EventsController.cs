9c566137fb5c3264be26a4c06a9d4736895c5149
Vidyakina ES <VidyakinaES@quarta.su>
2016-12-08 16:09:39 +0300

    moved clinet to one folder and backend to another

---
+++ FridayNight.Web/src/FridayNight.Web/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GoOutOnFriday.Event;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GoOutOnFriday.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventsController : Controller
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Details.Request request = new Details.Request
+            {
+                Id = id
+            };
+
+            Details.Query query = new Details.Query();
+            var result = await query.HandleQueryAsync(request);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(result);
+        }
+    }
+}
